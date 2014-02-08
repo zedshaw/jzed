6d0a191eb4cfd390d67dd8e24ba0649a0be3c1c9
Zed A. Shaw <zedshaw@zedshaw.com>
2014-02-07 16:01:35 -0800

    First commit of shitty code.

--- README.md
+++ README.md
@@ -1,4 +1,143 @@
-jzed
+JZed.js -- The functional JQuery Alternative
 ====
 
-Javascript alternative to JQuery.
+JZed is my alternative to JQuery that's meant to be more functional and full of
+$.  It's legit purpose is to do the 80% of things that you sometimes need to do
+on a remote site that is punching through to yours and they might have a
+different or no JQuery installed.  This is a tiny amount of code that has
+enough of the functionality found in JQuery and packaged in a way that can't
+possibly clash with it.
+
+It's also functional so now LISP hackers can pretend they're both JavaScript
+Oakland Living Node.js gangsters and high class San Franciscans with their
+functionality snobbery.
+
+How It Works
+============
+
+There's a bunch of functions that start with $ and do stuff that's super
+useful and all start with $. This way they probably won't clash with anything that exists already (let me know if I made one that does, or just change it).
+
+
+Getting
+-------
+
+Each of these takes an optional argument of inChildren at which point it
+will scope the getting inside those children.
+
+  * $get(name, inChildren) -- Get node by selector.
+  * $all(name, inChildren) -- All nodes by selector.
+  * $id(name, inChildren) -- Nodes with id.
+  * $class(name) -- Nodes with that class.
+  * $name(name, inChildren) -- Nodes with name.
+  * $filter(nodes, fn) -- Filters by true/false function.
+
+Looping
+-------
+
+  * $each(arr, cb) -- Call cb on each in arr.
+  * $next(node) -- Get the next sibling.
+  * $previous(node) -- Get the previous sibling.
+  * $siblings(node) -- Get all the siblings.
+  * $map(arr, fn) -- Map over an array.
+
+
+Styling
+-------
+
+  * $style() -- Takes unlimited arguments of [nodes, className] and loops through them applying that CSS class to those elements.  Nodes can be single or lists.
+  * $toggle(node, className) -- Turns a class on off.
+  * $has\_class(node, className) -- Tells you if it has that class.
+  * $style\_of(node, ruleName) -- Tells you the style of that node.
+
+
+DOMing
+------
+
+  * $new(tag, id, html) -- Make a new tag with that id and html in it.
+  * $after(node, html) -- Put that html after this node.
+  * $before(node, html) -- Put that html before this node.
+  * $append(parent, child) -- Append that child after this parent.
+  * $prepend(parent, node) -- Prepend that node before this parent.
+  * $remove(parent, child) -- Remove that child from this parent.
+  * $clone(node) -- Clone that node.
+  * $parent(node) -- Get the parent of this node.
+
+Testing
+-------
+
+  * $matches(node, selector) -- Does node match this selector.
+  * $contains(node, child) -- Does node contain that child.
+  * $has(node, selector) -- Does node have this selector?
+  * $empty(node) -- Is this node empty.
+  * $attribute\_of(node, name) -- Get the attribute named from the node.
+  * $attribute(node, name, value) -- Set the attribute name in node to value.
+
+HTMLing
+-------
+
+  * $html\_of(node) -- Get the HTML of node.
+  * $html(node, newhtml) -- Set the HTML of node to newhtml.
+  * $outer\_html(node) -- Get the outer HTML of node.
+  * $replace\_with(node, newhtml) -- Replace node with newhtml.
+  * $text(node, newtext) -- Set the text of node to newtext.
+  * $offset(node) -- Get node's offset.
+  * $offset\_parent(node) -- Get the parent offset of node.
+
+Eventing
+--------
+
+  * $boot(cb) -- Call cb function when the browser is ready.
+  * $off(node, eventName,  eventHandler) -- Turn off/remove eventName and eventHandler.
+  * $on(node, eventName, eventHandler) -- Turn on/add eventName and eventHandler.
+
+HTTPing
+-------
+
+  * $http(settings) -- Send an http request using the attributes: credentials, good, error, method, url. Go read the code.
+  * $parse(htmlString) -- Parse the given HTML into a new node.
+  * $json(jsonString) -- Parse the JSON.
+
+Miscing
+-------
+
+  * $now() -- Get the current time.
+
+
+License
+=======
+
+MIT. I seriously do not care about this apart from releasing it in case someone
+else could use it.
+
+
+Status
+======
+
+I use some of these functions and know they work, but I've been a mega slacker
+lately and need to test all the others.  I'm posting it now so people can laugh
+at it and try it out if they feel brave.
+
+This also only really works on IE9 and maybe IE8 or better, but honstly IE6
+and IE7 users users can seriously go fuck themselves with a dirty brick.
+I may add a function to detect these two bad browser so people can start
+forcing them to use a real browser for a change.
+
+Why?
+====
+
+I'm doing a bunch of work with CORS and requiring target sites to use jquery is
+insane when I just need a few little things.  Originally the library was about
+10 functions, but then I filled it out with more based on the list at
+http://youmightnotneedjquery.com/ thanks to @zackbloom and @adamfschwartz.
+
+I picked JZed both to annoy people and because it's got lots of $$$ in it
+so I predict it'll have more problems, yet less code.
+
+Thanks
+======
+
+As mentioned, thanks to @zackbloom and @adamfschwartz for their nice page
+cataloging things people use in JQuery and providing little snippets
+for me to borrow.
+
---
+++ jzed.js
@@ -0,0 +1,235 @@
+/**
+ * JZED.js -- The functional jquery alternative full of cash money.
+ *
+ * It's MIT licensed and a bunch of the code was lifted from http://youmightnotneedjquery.com/ but
+ * I have had this library floating around for a while.
+ *
+ * Do not use this in production, it hasn't been fully tested. It will eventually.
+ */
+
+function $get(name, inChildren) {
+    if(inChildren) {
+        return inChildren.querySelector(name);
+    } else {
+        return document.querySelector(name);
+    }
+}
+
+function $all(name, inChildren) {
+    if(inChildren) {
+        return inChildren.querySelectorAll(name);
+    } else {
+        return document.querySelectorAll(name);
+    }
+}
+
+function $id(name, inChildren) {
+    if(inChildren) {
+        return inChildren.children.namedItem(name);
+    } else {
+        return document.getElementById(name);
+    }
+}
+
+function $class(name) {
+    return document.getElementsByClassName(name);
+}
+
+function $name(name, inChildren) {
+    if(inChildren) {
+        return inChildren.children.namedItem(name);
+    } else {
+        return document.getElementsByTagName(name);
+    }
+}
+
+function $filter(nodes, fn) {
+    return Array.prototype.filter.call(nodes, fn);
+}
+
+
+function $each(arr, cb) {
+    if(arr == null) {
+        return;
+    } else if (Array.prototype.forEach && arr.forEach === Array.prototype.forEach) {
+        arr.forEach(cb);
+    } else {
+        for(var i = 0; i < arr.length; i++) {
+            (function() {
+                cb(arr[i], i, arr);
+            })();
+        }
+    }
+}
+
+function $next(node) {
+    node.nextElementSibling;
+}
+
+function $previous(node) {
+    node.previousElementSibling;
+}
+
+function $siblings(node) {
+    return $filter(node.parentNode.children, function (child) { return child !== el; });
+}
+
+function $style() {
+    $each(arguments, function(val) {
+        if(val[0].length != null) {
+            $each(val[0], function(element) {
+                element.className = val[1];
+            });
+        } else {
+            val[0].className = val[1];
+        }
+    });
+}
+
+function $toggle(node, className) {
+  node.classList.toggle(className);
+}
+
+function $new(tag, id, html) {
+    var new_tag = document.createElement("div");
+    new_tag.id = id;
+    new_tag.innerHTML = html;
+    return new_tag;
+}
+
+function $after(node, html) {
+    node.insertAdjacentHTML('afterend', html);
+}
+
+function $before(node, html) {
+    node.insertAdjacentHTML('beforebegin', html);
+}
+
+function $append(parent, child) {
+    parent.appendChild(child);
+}
+
+function $prepend(parent, node) {
+    parent.insertBefore(node, parent.firstChild);
+}
+
+function $remove(parent, child) {
+    parent.removeChild(child);
+}
+
+function $clone(node) {
+    return node.cloneNode(true);
+}
+
+function $contains(node, child) {
+    return node !== child && node.contains(child);
+}
+
+function $has(node, selector) {
+    return node.querySelector(selector) !== null;
+}
+
+function $empty(node) {
+    node.innerHTML = '';
+}
+
+function $style_of(node, ruleName) {
+    return getComputedStyle(node)[ruleName]
+}
+
+function $attribute_of(node, name) {
+    return node.getAttribute(name);
+}
+
+function $attribute(node, name, value) {
+    return node.setAttribute(name, value);
+}
+
+function $html_of(node) {
+    return node.innerHTML;
+}
+
+function $html(node, newhtml) {
+    node.innerHTML = newhtml;
+}
+
+function $has_class(node, className) {
+  return node.classList.contains(className);
+}
+
+
+function $outer_html(node) {
+    return node.outerHTML;
+}
+
+function $replace_with(node, newhtml) {
+    node.outerHTML = string;
+}
+
+function $matches(node, selector) {
+    return (node.matches || node.matchesSelector || node.msMatchesSelector || node.mozMatchesSelector || node.webkitMatchesSelector || node.oMatchesSelector)(selector);
+}
+
+function $offset(node) {
+    return {left: node.offsetLeft, top: node.offsetTop};
+}
+
+function $offset_parent(node) {
+    return node.offsetParent || node;
+}
+
+function $parent(node) {
+    return node.parentNode;
+}
+
+function $text(node, newtext) {
+    node.textContent = newtext;
+}
+
+function $boot(cb) {
+    document.addEventListener("DOMContentLoaded", cb);
+}
+
+function $off(node, eventName,  eventHandler) {
+    node.removeEventListener(eventName, eventHandler);
+}
+
+function $on(node, eventName, eventHandler) {
+    node.addEventListener(eventName, eventHandler);
+}
+
+function $http(settings) {
+    var req = new XMLHttpRequest();
+    req.withCredentials = settings.credentials;
+
+    req.onreadystatechange = function(){
+        if(req.readyState == 4) {
+            if (req.status == 200){
+                settings.good(req);
+            } else {
+                settings.error(req);
+            }
+        }
+    }
+    req.open(settings.method, settings.url, true);
+    req.send(null);
+}
+
+function $map(arr, fn) {
+    return arr.map(fn);
+}
+
+function $now() {
+    return Date.now();
+}
+
+function $parse(htmlString) {
+    var tmp = document.implementation.createHTMLDocument()
+    tmp.body.innerHTML = str
+    return tmp.body.children;
+}
+
+function $json(jsonString) {
+    return JSON.parse(jsonString);
+}
+
